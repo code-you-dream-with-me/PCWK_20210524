5a6cd16c1ddb41615a6c28e2662dd890a503033d
김현룡 <82699587+code-you-dream-with-me@users.noreply.github.com>
2021-05-25 21:34:41 +0900

    Add files via upload

---
+++ First/src/chapter2/CharacterEx4.java
@@ -0,0 +1,26 @@
+package chapter2;
+
+public class CharacterEx4 {
+
+	public static void main(String[] args) {
+		char   ch1 ='Z';
+		
+		System.out.println(ch1);
+		System.out.println((int)ch1);
+		
+		char   ch2 =38;
+		System.out.println(ch2);
+		System.out.println((int)ch2);
+		
+		int ch3 = 97;
+		System.out.println(ch3);
+		System.out.println((char)ch3);		
+	}
+
+}
+//Z
+//90
+//&
+//38
+//97
+//a
