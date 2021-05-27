1746915cb906904652dd2524d2cda783275e77a4
김현룡 <82699587+code-you-dream-with-me@users.noreply.github.com>
2021-05-27 21:58:22 +0900

    Update SwitchCase3.java

--- Chapter4/src/ifexample/SwitchCase3.java
+++ Chapter4/src/ifexample/SwitchCase3.java
@@ -21,6 +21,6 @@ public class SwitchCase3 {
 				medalColor = 'A';
 		}
 		
-		System.out.println(ranking + "µî ¸Ş´ŞÀÇ »ö±òÀº " + medalColor + " ÀÔ´Ï´Ù.");
+		System.out.println(ranking + "ë“± ë©”ë‹¬ì˜ ìƒ‰ê¹”ì€ " + medalColor + " ì…ë‹ˆë‹¤.");
 	}
 }
